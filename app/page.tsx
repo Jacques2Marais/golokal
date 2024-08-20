d0a33cc197f19c93bd2b41479c2cf9390aa6af12
Jacques Marais <iamjacquesmarais@gmail.com>
2024-08-20 16:12:29 +0200

    Decrease main hero video blur

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ function HomeHero () {
   return (
     <>
       <section className="flex flex-col items-center justify-center lg:py-72 md:py-56 py-32 gap-12 bg-secondary">
-        <video playsInline autoPlay muted loop poster="/main-video-poster.jpeg" className="absolute w-full h-full brightness-75 blur-sm">
+        <video playsInline autoPlay muted loop poster="/main-video-poster.jpeg" className="absolute w-full h-full brightness-75 blur-[2px]">
           {/* <source src="polina.webm" type="video/webm" /> */}
           <source src="/main-video-comp.mp4" type="video/mp4" />
         </video>
